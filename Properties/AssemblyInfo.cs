e53e7e9772cd2a28abcaa911ddb277dbc085bfd4
Vin-meido <86288831+Vin-meido@users.noreply.github.com>
2025-10-25 22:31:16 +0800

    GitHub actions (#4)
    * Update to use dotnet build + github actions

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,16 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using COM3D2.UndressUtil.Plugin;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UndresUtil")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UndresUtil")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,16 +12,3 @@ using COM3D2.UndressUtil.Plugin;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("09c4afb0-39b9-4bb9-a1d2-53c034157375")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion(Version.NUMBER)]
-[assembly: AssemblyFileVersion(Version.NUMBER)]
